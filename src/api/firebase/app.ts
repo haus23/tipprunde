d0b4f108a119ce6b2808021bdaf34de577ec7d9e
Micha Alt <micha@haus23.net>
2022-03-04 14:34:21 +0100

    Initialize firebase app.

---
+++ src/api/firebase/app.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+
+export const app = initializeApp({
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
+});
