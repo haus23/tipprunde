7ee9f8c60f16731da23c4b6deab6f32cd88d3dc8
Micha Alt <micha@haus23.net>
2022-03-04 14:40:26 +0100

    Usage of async state with suspense.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useChampionships } from './api/hooks/use-championships';
+
 function App() {
+  const { championships } = useChampionships();
+
   return (
     <div>
       <h1>runde.tips</h1>
+      <h2>
+        {championships.length ? championships[0].title : 'Noch kein Turnier'}
+      </h2>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <Suspense fallback={<p>Loading ...</p>}>
+        <App />
+      </Suspense>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
