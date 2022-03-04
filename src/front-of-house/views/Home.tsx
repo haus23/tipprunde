c057b43aaca21ab45ec1eab54b6711f29327ff88
Micha Alt <micha@haus23.net>
2022-03-04 19:57:13 +0100

    Add proper homescreen.

--- src/front-of-house/views/Home.tsx
+++ src/front-of-house/views/Home.tsx
@@ -1,12 +1,45 @@
-import { useChampionships } from '@/api/hooks/use-championships';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '@/api/hooks/use-auth';
+
+import Button from '@/ui/atoms/Button';
 
 function Home() {
-  const { championships } = useChampionships();
+  const { isAuthenticated } = useAuth();
+
+  const navigate = useNavigate();
   return (
-    <div>
-      <h2>
-        {championships.length ? championships[0].title : 'Noch kein Turnier'}
-      </h2>
+    <div className="flex flex-col gap-y-4 rounded-md bg-white p-4 shadow-md dark:bg-gray-800">
+      <h2 className="px-4 text-3xl font-semibold">Haus23 Tipprunde</h2>
+      <hr />
+      <div className="px-4 text-lg">
+        Hier entsteht unsere neue Tipprunde - also zumindest die
+        Online-Auswertung. Bis allerdings erste Tabellenstände tatsächlich hier
+        auf Abruf stehen, dauert es noch eine kleine Weile.
+        <div className="mt-8 flex flex-col items-center gap-y-4">
+          {isAuthenticated ? (
+            <Button
+              primary
+              onClick={() => navigate('hinterhof')}
+              className="w-40"
+            >
+              Zum Hinterhof
+            </Button>
+          ) : (
+            <>
+              <Button
+                primary
+                onClick={() => navigate('login')}
+                className="w-32"
+              >
+                Log In
+              </Button>
+              <div className="text-sm text-gray-500">
+                Nur für interne Zwecke
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/ui/atoms/Button.tsx
@@ -0,0 +1,29 @@
+import { ComponentPropsWithoutRef, forwardRef, ReactNode } from 'react';
+import { classNames } from '@/core/helpers/class-names';
+
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+  children?: ReactNode;
+  primary?: boolean;
+};
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, primary = false, className, ...props }: ButtonProps, ref) => (
+    <button
+      ref={ref}
+      type="button"
+      {...props}
+      className={classNames(
+        'inline-flex items-center justify-center rounded-md border px-4 py-2 text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800',
+        primary
+          ? 'border-transparent bg-indigo-600 text-white hover:bg-indigo-700'
+          : 'border-gray-300 bg-white text-gray-700 hover:bg-gray-50 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700',
+        className ?? ''
+      )}
+    >
+      {children || 'Button'}
+    </button>
+  )
+);
+Button.displayName = 'Button';
+
+export default Button;
