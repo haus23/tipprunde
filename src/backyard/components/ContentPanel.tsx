45c75a13c6f5f1305834e2ebed9533e297a17ba3
Micha Alt <micha@haus23.net>
2022-03-05 15:46:01 +0100

    Update backyard dashboard.

---
+++ src/backyard/components/ContentPanel.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+type ContentPanelProps = {
+  title: ReactNode;
+  children?: ReactNode;
+};
+
+const ContentPanel = ({ title, children }: ContentPanelProps) => (
+  <div>
+    <div className="px-4 h-14 md:h-16 absolute md:static top-0 left-10 sm:left-12 z-20 md:border-b border-gray-300 dark:border-gray-700 flex items-center">
+      <h2 className="text-2xl font-semibold">{title}</h2>
+    </div>
+    <div className="py-4 px-4 md:px-8">{children}</div>
+  </div>
+);
+
+export default ContentPanel;
