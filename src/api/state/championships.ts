f7214b51f9f87701b0b3e7ab65bf788a9628a6a4
Micha Alt <micha@haus23.net>
2022-03-05 22:49:06 +0100

    Ordering by nr descending.

--- src/api/state/championships.ts
+++ src/api/state/championships.ts
@@ -1,8 +1,9 @@
+import { orderBy } from 'firebase/firestore';
 import { atom } from 'recoil';
 import { Championship } from '../model/championship';
 import { asyncList } from '../model/repository/async-list';
 
 export const championshipsState = atom({
   key: 'championships',
-  default: asyncList<Championship>('championships'),
+  default: asyncList<Championship>('championships', orderBy('nr', 'desc')),
 });
