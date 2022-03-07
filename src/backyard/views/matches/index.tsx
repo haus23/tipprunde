1eb1abac0546b086c4e769d100c5f6645866e892
Micha Alt <micha@haus23.net>
2022-03-07 20:56:13 +0100

    Add notification service.

--- src/backyard/views/matches/index.tsx
+++ src/backyard/views/matches/index.tsx
@@ -2,16 +2,19 @@ import { useLeagues } from '@/api/hooks/use-leagues';
 import { League } from '@/api/model/league';
 import ContentPanel from '@/backyard/components/ContentPanel';
 import LeagueForm from '@/backyard/components/forms/LeagueForm';
+import { useNotify } from '@/core/hooks/use-notify';
 
 function Matches() {
   const { create } = useLeagues();
-  const save = (league: League) => {
-    console.log('Neue Liga', league);
+  const notify = useNotify();
+
+  const handleCreate = (league: League) => {
+    notify(create(league), `Neue Liga${league.name}`);
   };
 
   return (
     <ContentPanel title="Spiele">
-      <LeagueForm onSave={create} onCancel={() => {}} />
+      <LeagueForm onSave={handleCreate} onCancel={() => {}} />
     </ContentPanel>
   );
 }
