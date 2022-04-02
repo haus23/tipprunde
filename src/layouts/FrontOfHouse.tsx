d42cce13006286f7dc5e819bd6ef6c4c181d50a1
Micha Alt <micha@haus23.net>
2022-04-02 11:30:03 +0200

    Implement FoH layout header.

--- src/layouts/FrontOfHouse.tsx
+++ src/layouts/FrontOfHouse.tsx
@@ -1,11 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import FoHHeader from './components/FoHHeader';
 
 export default function FrontOfHouse() {
   return (
     <>
-      <h1 className="text-2xl font-semibold m-4 border-gray-300 border-b">
-        runde.tips
-      </h1>
+      <FoHHeader />
       <main className="sm:mt-4 max-w-3xl mx-auto p-4">
         <Outlet />
       </main>
