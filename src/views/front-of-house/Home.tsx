0820e35d29f2315d94bfea3ce75c02557574b190
Micha Alt <micha@haus23.net>
2022-04-02 17:57:32 +0200

    Update home view.

--- src/views/front-of-house/Home.tsx
+++ src/views/front-of-house/Home.tsx
@@ -1,10 +1,23 @@
-import { Link } from 'react-router-dom';
+import Button from '@/components/atoms/Button';
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-y-4 rounded-md bg-white p-4 shadow-md dark:bg-gray-800">
       <h2 className="px-4 text-3xl font-semibold">Haus23 Tipprunde</h2>
-      <Link to="/hinterhof">Hinterhof</Link>
+      <hr />
+      <div className="px-4 text-lg">
+        Hier entsteht unsere neue Tipprunde - also zumindest die
+        Online-Auswertung. Bis allerdings erste Tabellenstände tatsächlich hier
+        auf Abruf stehen, dauert es noch eine kleine Weile.
+        <div className="mt-8 flex flex-col items-center gap-y-4">
+          <>
+            <Button primary className="w-32">
+              Log In
+            </Button>
+            <div className="text-sm text-gray-500">Nur für interne Zwecke</div>
+          </>
+        </div>
+      </div>
     </div>
   );
 }
