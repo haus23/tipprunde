78c1285c86ec4d842c2c1129ceeb52cf40a19203
Micha Alt <micha@haus23.net>
2022-04-02 12:16:24 +0200

    Install and configure Recoil for state management.

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,14 @@ import 'tailwindcss/tailwind.css';
 
 // App
 import App from '@/App';
+import { RecoilRoot } from 'recoil';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </StrictMode>
 );
