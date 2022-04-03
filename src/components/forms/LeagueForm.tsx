d320f604b652d1f0ad969e7b8a58112b0d9892d2
Micha Alt <micha@haus23.net>
2022-04-03 14:44:42 +0200

    Implement basic league form.

---
+++ src/components/forms/LeagueForm.tsx
@@ -0,0 +1,66 @@
+import { useForm } from 'react-hook-form';
+import { League } from '@/api/model/league';
+import { useLeaguesSynced } from '@/api/hooks/use-leagues-synced';
+
+import TextField from '../atoms/TextField';
+import Button from '@/components/atoms/Button';
+
+type LeagueFormProps = {
+  onDone: () => void;
+};
+
+export default function LeagueForm({ onDone }: LeagueFormProps) {
+  const { leagues, create } = useLeaguesSynced();
+
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<League>();
+
+  const onSave = async (league: League) => {
+    await create(league);
+    onDone();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSave)}>
+      <div className="grid grid-cols-6 gap-4">
+        <TextField
+          className="col-span-6"
+          label="Bezeichnung"
+          errorMsg={errors.name?.message}
+          {...register('name', {
+            required: 'Pflichtfeld',
+            validate: {
+              uniqueName: (name) =>
+                !leagues.some((c) => c.name === name) ||
+                'Liga mit diesem Namen ist schon angelegt.',
+            },
+          })}
+        />
+        <TextField
+          className="col-span-6"
+          label="Kurzform"
+          errorMsg={errors.shortName?.message}
+          {...register('shortName', {
+            required: 'Pflichtfeld',
+            validate: {
+              uniqueShortname: (shortName) =>
+                !leagues.some((c) => c.shortName === shortName) ||
+                'Diese Kurzbezeichnung gibt es schon.',
+            },
+          })}
+        />
+        <div className="col-span-6 mt-4 flex justify-end gap-x-4">
+          <Button onClick={onDone} type="button">
+            Abbrechen
+          </Button>
+          <Button primary type="submit">
+            Anlegen
+          </Button>
+        </div>
+      </div>
+    </form>
+  );
+}
