3ffb151f77a4754158128111616225e3538734c8
Micha Buchholz <micha@haus23.net>
2024-12-09 08:47:47 +0100

    chore: Create initial React Router 7 app.

---
+++ app/routes/foh/tables/_route.tsx
@@ -0,0 +1,11 @@
+export const meta = [
+  { title: 'Tabelle - runde.tips' },
+  {
+    name: 'description',
+    content: 'Aktueller Tabellenstand der Haus23 Tipprunde',
+  },
+];
+
+export default function TablesRoute() {
+  return <h1>Aktuelle Tabelle</h1>;
+}
