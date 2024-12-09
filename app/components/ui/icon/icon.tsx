662de6ae9963be547f69924ff00757f6b6302830
Micha Buchholz <micha@haus23.net>
2024-12-09 17:58:25 +0100

    feat: Add icons component.

---
+++ app/components/ui/icon/icon.tsx
@@ -0,0 +1,53 @@
+import { tv } from '#/utils/tv';
+import iconsHref from './icons.svg';
+
+type IconName =
+  | 'calendar'
+  | 'chevrons-up-down'
+  | 'dices'
+  | 'folder'
+  | 'folder-sync'
+  | 'folders'
+  | 'house'
+  | 'log-out'
+  | 'menu'
+  | 'moon'
+  | 'pilcrow'
+  | 'scale'
+  | 'shield'
+  | 'smile-plus'
+  | 'trophy'
+  | 'user'
+  | 'users'
+  | 'x';
+
+const styles = tv({ base: 'inline size-5' });
+
+namespace Icon {
+  export interface Props extends React.SVGProps<SVGSVGElement> {
+    name: IconName;
+  }
+}
+
+function Icon({ name, className, children, ...props }: Icon.Props) {
+  if (children) {
+    return (
+      <span className="flex items-center justify-center gap-2">
+        <Icon name={name} className={className} {...props} />
+        {children}
+      </span>
+    );
+  }
+  return (
+    <svg
+      {...props}
+      role="img"
+      aria-label={`${name} icon`}
+      className={styles({ className })}
+    >
+      <use href={`${iconsHref}#${name}`} />
+    </svg>
+  );
+}
+
+export { Icon, type IconName };
