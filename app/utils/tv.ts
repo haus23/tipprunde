b9f003261f57f84e5310666db130c4f941958590
Micha Buchholz <micha@haus23.net>
2024-12-09 22:14:52 +0100

    chore: Add low-level libs.

--- app/utils/tv.ts
+++ app/utils/tv.ts
@@ -1,4 +1,7 @@
+import { extendTailwindMerge } from 'tailwind-merge';
 import { createTV } from 'tailwind-variants';
+
+export const twMerge = extendTailwindMerge({});
 
 export type { VariantProps } from 'tailwind-variants';
 export const tv = createTV({});
