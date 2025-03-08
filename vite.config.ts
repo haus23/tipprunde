a45c9910d51c150d9c2117cbe1acbcf491966203
Micha Buchholz <micha@haus23.net>
2025-03-08 20:39:18 +0100

    chore: Prepare cloudflare deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import type { UserConfig } from 'vite';
 
 // Vite Plugins
 import { reactRouter } from '@react-router/dev/vite';
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare';
 
 export default {
-  plugins: [reactRouter()],
+  plugins: [
+    cloudflareDevProxy({
+      getLoadContext({ context }) {
+        return { cloudflare: context.cloudflare };
+      },
+    }),
+    reactRouter(),
+  ],
 } satisfies UserConfig;
