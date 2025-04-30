09f1f4932cdfb6542fa52460d90014cb6c774a04
Micha Buchholz <micha@haus23.net>
2025-04-30 10:05:27 +0200

    chore: Add cloudflare integration via vite plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import {defineConfig} from 'vite';
 
 // Plugins
 import {reactRouter} from '@react-router/dev/vite';
+import {cloudflare} from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-    plugins: [reactRouter()],
+    plugins: [cloudflare({ viteEnvironment: { name: 'ssr' }}), reactRouter()],
 })
