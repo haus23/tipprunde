218be76caa82f6c9ab39ebc346feab400331eb18
Micha Buchholz <micha@haus23.net>
2025-04-30 10:51:38 +0200

    feat: Add cloudflare var and load the current championship from unterbau.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,22 @@
+import type { Route } from "./+types/index";
+
 export function meta() {
     return [
         { title: "runde.tips" }
     ];
 }
 
-export default function Home() {
+export async function loader({ context }: Route.LoaderArgs) {
+    const response = await fetch(`${context.cloudflare.env.UNTERBAU_URL}/api/v1/championships`);
+    const data = (await response.json()) as any;
+    return { championship: data[0] };
+}
+
+export default function Home({loaderData}: Route.ComponentProps) {
     return (
         <div className="p-2">
             <h1 className="text-2xl font-medium">runde.tips</h1>
+            <h2 className="text-2xl font-medium">{loaderData.championship.name}</h2>
         </div>
     );
 }
