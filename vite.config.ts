ece605d21b60ea704f1d68803800bd8812ca9818
Micha Buchholz <micha@haus23.net>
2025-05-09 15:28:36 +0200

    fix: Add vite plugin to handle chrome devtool requests.

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import { defineConfig } from 'vite';
 import { cloudflare } from '@cloudflare/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindCss from '@tailwindcss/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    devtoolsJson(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindCss(),
     reactRouter(),
