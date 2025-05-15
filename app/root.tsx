f51b76b6b11457670208ee6dfce6b319f6478b54
Micha Buchholz <micha@haus23.net>
2025-05-15 21:08:51 +0200

    refactor: Change color-scheme switching.

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   useAuthBroadcast();
 
   return (
-    <html lang="de" data-theme="mauvi">
+    <html lang="de" className='system' data-theme="mauvi">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
