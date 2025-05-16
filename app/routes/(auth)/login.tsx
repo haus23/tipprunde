cccad0eda8e642406d4879bab8b5ddfe1fdbaf2b
Micha Buchholz <micha@haus23.net>
2025-05-16 11:27:41 +0200

    refactor: Adjust styles for the now responsive design.

--- app/routes/(auth)/login.tsx
+++ app/routes/(auth)/login.tsx
@@ -33,26 +33,28 @@ export default function Login({ actionData }: Route.ComponentProps) {
   };
 
   return (
-    <div className="mx-auto mt-12 flex max-w-md flex-col gap-y-6 rounded-md border border-app-6 p-4 shadow-md">
-      <h1 className="font-medium text-2xl">Log In</h1>
-      <Form
-        method="post"
-        onSubmit={onSubmit}
-        validationErrors={actionData?.errors}
-        className="flex flex-col gap-y-4"
-      >
-        <TextField
-          name="email"
-          type="email"
-          isRequired
-          label="Email"
-          placeholder="Deine Email-Adresse aus der Tipprunde"
-        />
-        <Checkbox name="rememberMe">Angemeldet bleiben</Checkbox>
-        <Button type="submit" variant="primary">
-          Login-Code anfordern
-        </Button>
-      </Form>
+    <div className="px-2 sm:px-0">
+      <div className="mx-auto flex max-w-md flex-col gap-y-6 rounded-md border border-app-6 p-4 shadow-md">
+        <h1 className="font-medium text-2xl">Log In</h1>
+        <Form
+          method="post"
+          onSubmit={onSubmit}
+          validationErrors={actionData?.errors}
+          className="flex flex-col gap-y-4"
+        >
+          <TextField
+            name="email"
+            type="email"
+            isRequired
+            label="Email"
+            placeholder="Adresse aus der Tipprunde"
+          />
+          <Checkbox name="rememberMe">Angemeldet bleiben</Checkbox>
+          <Button type="submit" variant="primary">
+            Login-Code anfordern
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
