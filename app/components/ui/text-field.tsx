8d5ca9674eb9de4621b77d5c034110598a2a5a4d
Micha Buchholz <micha@haus23.net>
2025-05-27 10:49:19 +0200

    feat: Make text-field more customizable.

--- app/components/ui/text-field.tsx
+++ app/components/ui/text-field.tsx
@@ -1,32 +1,44 @@
 import type { TextFieldProps as _TextFieldProps } from 'react-aria-components';
 
 import { TextField as _TextField } from 'react-aria-components';
-import { tv } from 'tailwind-variants';
+import {tv, type VariantProps} from 'tailwind-variants';
 
 import { FieldError } from '~/components/ui/field-error';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
 
 const styles = tv({
-  base: ['flex flex-col gap-y-2'],
+  base: ['flex'],
+  variants: {
+    orientation: {
+      horizontal: ['flex-row items-center gap-x-2'],
+      vertical: ['flex-col gap-y-2'],
+    },
+  },
+  defaultVariants: {
+    orientation: 'vertical',
+  },
 });
 
-interface TextFieldProps extends _TextFieldProps {
+interface TextFieldProps extends _TextFieldProps, VariantProps<typeof styles> {
   label: string;
+  labelClasses?: string;
   className?: string;
   placeholder?: string;
 }
 
 export function TextField({
   label,
+  labelClasses,
   className,
   placeholder,
+  orientation,
   ...props
 }: TextFieldProps) {
   return (
-    <_TextField {...props} className={styles({ className })}>
-      <Label>{label}</Label>
-      <Input placeholder={placeholder} />
+    <_TextField {...props} className={styles({ orientation, className })}>
+      <Label className={labelClasses}>{label}</Label>
+      <Input placeholder={placeholder} className="grow" />
       <FieldError className="text-sm" />
     </_TextField>
   );
