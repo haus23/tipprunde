e089dd1f188f97be1afa1c9cbdaa915e7466c093
Micha Buchholz <micha@haus23.net>
2025-05-27 16:36:41 +0200

    feat: Add drizzle-valibot to be able to validate db data.

--- app/utils/db/user.ts
+++ app/utils/db/user.ts
@@ -1,3 +1,7 @@
+import type { UserInsert } from '~/database/types';
+
+import { eq } from 'drizzle-orm';
+
 import app from '~/app';
 import { users } from '~/database/schema';
 import { env } from '~/utils/env.server';
@@ -9,7 +13,38 @@ const rootUser = {
   roles: 'ROOT ADMIN',
 };
 
-export async function createOrUpdateUser(props: typeof users.$inferInsert) {
+/**
+ * Creates a new user.
+ *
+ * @param user - The user data to insert. Must conform to the structure defined by `users.$inferInsert`.
+ * @return A promise that resolves when the operation is completed.
+ */
+export async function createUser(user: UserInsert) {
+  const { db } = app;
+  return db.insert(users).values(user);
+}
+
+/**
+ * Updates a user's information in the database.
+ *
+ * @param id - The user id - primary key
+ * @param user - The user object containing updated information. The `id` property is used to locate the user in the database.
+ * @return A promise that resolves to the result of the database update operation.
+ */
+export async function updateUser(id: number, user: UserInsert) {
+  const { db } = app;
+  return db.update(users).set(user).where(eq(users.id, id));
+}
+
+/**
+ * Creates a new user or updates an existing user based on the provided data.
+ * If a conflict occurs on the `slug` field, the existing record will be updated with the new data.
+ * This method is mainly used for syncing the legacy users
+ *
+ * @param props - The user data to insert or update. Must conform to the structure defined by `users.$inferInsert`.
+ * @return A promise that resolves when the operation is completed.
+ */
+export async function createOrUpdateUser(props: UserInsert) {
   const { db } = app;
   return db.insert(users).values(props).onConflictDoUpdate({
     target: users.slug,
--- database/types.ts
+++ database/types.ts
@@ -1,3 +1,13 @@
-import type { users } from '~/database/schema';
+import { createInsertSchema } from 'drizzle-valibot';
+import * as v from 'valibot';
+
+import { users } from '~/database/schema';
+
+export const userInsertSchema = createInsertSchema(users, {
+  id: v.optional(
+    v.union([v.number(), v.pipe(v.string(), v.transform(Number))]),
+  ),
+});
 
 export type User = typeof users.$inferSelect;
+export type UserInsert = typeof users.$inferInsert;
