5f34d48336c60037f6a45b0b52fc69badcf0c5a3
Micha Buchholz <micha@haus23.net>
2025-05-27 15:08:57 +0200

    feat: Add disabled styles.

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -7,6 +7,7 @@ const styles = tv({
   base: [
     'rounded-md border border-app-7 px-3 py-1.5',
     'outline-hidden ring-offset-2 ring-offset-app-1 data-[focused=true]:ring-2 data-[focused=true]:ring-info-11',
+    'disabled:pointer-events-none disabled:opacity-50',
   ],
 });
 
