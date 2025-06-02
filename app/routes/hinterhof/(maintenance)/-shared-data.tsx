f170f645a05a2379ffc124d6a12caf314b351830
Micha Buchholz <micha@haus23.net>
2025-06-02 13:19:18 +0200

    feat: Add static data placeholder.

--- app/routes/hinterhof/(maintenance)/-shared-data.tsx
+++ app/routes/hinterhof/(maintenance)/-shared-data.tsx
@@ -5,6 +5,35 @@ import { useFetcher } from 'react-router';
 
 import { Button } from '~/components/ui/button';
 
+export function SharedDataPlaceholder() {
+  return (
+    <div className="mt-4 flex animate-pulse flex-col gap-y-4">
+      <div className="flex flex-col gap-y-4 px-4">
+        <p class="space-y-3">
+          <div className="h-2 rounded bg-app-4" />
+          <div className="grid grid-cols-3 gap-4">
+            <div className="col-span-2 h-2 rounded bg-app-4" />
+            <div className="col-span-1 h-2 rounded bg-app-4" />
+          </div>
+        </p>
+        <p class="space-y-3">
+          <div className="grid grid-cols-3 gap-4">
+            <div className="col-span-1 h-2 rounded bg-app-4" />
+            <div className="col-span-2 h-2 rounded bg-app-4" />
+          </div>
+          <div className="h-2 rounded bg-app-4" />
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-around gap-4">
+        <div className="h-[56px] w-32 rounded-md bg-app-4" />
+        <div className="h-[56px] w-32 rounded-md bg-app-4" />
+        <div className="h-[56px] w-32 rounded-md bg-app-4" />
+        <div className="h-[56px] w-32 rounded-md bg-app-4" />
+      </div>
+    </div>
+  );
+}
+
 export function SharedData({
   syncState,
 }: {
@@ -14,15 +43,29 @@ export function SharedData({
   const state = use(syncState);
 
   return (
-    <div>
+    <div className="mt-4 flex flex-col gap-y-4">
+      <div className="flex flex-col gap-y-2 px-4">
+        <p className="text-app-11">
+          Stammdaten sind die unveränderlichen Daten, die in den jeweiligen
+          Turnieren benutzt werden: Spieler, Mannschaften/Teams, Ligen und
+          Regelwerke. Diese Daten sind relativ stabil und ändern sich maximal
+          bei neuen Runden oder neuen Turnieren.
+        </p>
+        <p className="text-app-11">
+          Solange die Firestore-Datenbank maßgeblich die Daten besitzt, müssen -
+          nach Änderungen dieser - die lokalen Daten der Anwendung
+          synchronisiert werden.
+        </p>
+      </div>
       <fetcher.Form method="POST" action="/action/sync/shared-data">
-        <div className="flex gap-4">
+        <div className="flex flex-wrap justify-around gap-4">
           <Button
             isDisabled={!state.updatedResources.includes('users')}
             variant="primary"
             type="submit"
             name="intent"
             value="users"
+            className="w-32 justify-center py-4"
           >
             Spieler
           </Button>
@@ -32,6 +75,7 @@ export function SharedData({
             type="submit"
             name="intent"
             value="teams"
+            className="w-32 justify-center py-4"
           >
             Teams
           </Button>
@@ -41,6 +85,7 @@ export function SharedData({
             type="submit"
             name="intent"
             value="leagues"
+            className="w-32 justify-center py-4"
           >
             Ligen
           </Button>
@@ -50,6 +95,7 @@ export function SharedData({
             type="submit"
             name="intent"
             value="rulesets"
+            className="w-32 justify-center py-4"
           >
             Regelwerke
           </Button>
--- app/routes/hinterhof/(maintenance)/_route.tsx
+++ app/routes/hinterhof/(maintenance)/_route.tsx
@@ -2,7 +2,10 @@ import type { Route } from './+types/_route';
 
 import { Suspense } from 'react';
 
-import { SharedData } from '~/routes/hinterhof/(maintenance)/-shared-data';
+import {
+  SharedData,
+  SharedDataPlaceholder,
+} from '~/routes/hinterhof/(maintenance)/-shared-data';
 import { getSharedDataSyncState } from '~/utils/legacy-api/sync-state.server';
 import { requireAdmin } from '~/utils/user.server';
 
@@ -21,9 +24,9 @@ export default function MaintenanceRoute({ loaderData }: Route.ComponentProps) {
   const { sharedDataSyncState } = loaderData;
 
   return (
-    <div>
+    <div className="px-2 sm:px-0">
       <h1 className="font-medium text-2xl">Wartung</h1>
-      <Suspense fallback={<div>Waiting...</div>}>
+      <Suspense fallback={<SharedDataPlaceholder />}>
         <SharedData syncState={sharedDataSyncState} />
       </Suspense>
     </div>
