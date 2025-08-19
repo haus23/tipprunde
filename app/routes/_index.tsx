9f0896b845efef1d060052d54ee99691eba0be5a
Micha Buchholz <micha@haus23.net>
2025-08-20 00:18:43 +0200

    chore: Update index route to proof workflow concept.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div>
       <title>Tipprunde</title>
-      <h1>Welcome to Tipprunde</h1>
+      <h1>Willkommen bei der Tipprunde</h1>
       <p>Your minimal React Router app is ready!</p>
     </div>
   );
