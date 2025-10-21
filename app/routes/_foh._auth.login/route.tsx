05ec428b87e204676f6daed62c006ecd5b8938e6
Micha Buchholz <micha@haus23.net>
2025-10-21 14:27:00 +0200

    feat: Validate email address implemented.

--- app/routes/_foh._auth.login/route.tsx
+++ app/routes/_foh._auth.login/route.tsx
@@ -1,11 +1,32 @@
-import { useFetcher } from "react-router";
+import { data, useFetcher } from "react-router";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { TextField } from "~/components/ui/text-field";
 
+import type { Route } from "./+types/route";
+import { db } from "~/utils/db.server";
+import { redirect } from "react-router";
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const email = String(formData.get("email"));
+
+  const user = await db.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return data(
+      { errors: { email: "Unbekannte Email-Adresse. Wende dich an Micha." } },
+      { status: 400 },
+    );
+  }
+
+  return redirect("/verify");
+}
+
 export default function LoginRoute() {
   const fetcher = useFetcher();
+  const errors = fetcher.data?.errors;
 
   return (
     <div className="p-2 flex flex-col gap-4">
@@ -17,6 +38,7 @@ export default function LoginRoute() {
           <TextField>
             <Label htmlFor="email">Email</Label>
             <Input type="email" name="email" id="email" required />
+            {errors?.email ? <em className="text-sm">{errors.email}</em> : null}
           </TextField>
           <div>
             <Button type="submit">Code anfordern</Button>
---
+++ app/routes/_foh._auth.verify/route.tsx
@@ -0,0 +1,9 @@
+export default function VerifyRoute() {
+  return (
+    <div>
+      <title>Einlass - runde.tips</title>
+      <meta name="description" content="Einlass bei der Haus23 Tipprunde" />
+      <h1 className="p-2 text-2xl font-medium">Einlass</h1>
+    </div>
+  );
+}
