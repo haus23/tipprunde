9e236c9a3905271991597a16e019c7d39540eefa
Micha Buchholz <micha@haus23.net>
2025-10-27 14:20:28 +0100

    chore: Configure deployable nitro v3 server.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import type { UserConfig } from "vite";
 
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default {
-  plugins: [tsConfigPaths(), tanstackStart(), react()],
+  plugins: [tsConfigPaths(), tanstackStart(), nitro(), react()],
 } satisfies UserConfig;
