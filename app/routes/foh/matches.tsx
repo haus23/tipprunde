ac0a5e5f77aa639f343e2275b1c56c8381b70d60
Micha Buchholz <micha@haus23.net>
2025-11-03 13:03:19 +0100

    feat: Add initial foh routes.

---
+++ app/routes/foh/matches.tsx
@@ -0,0 +1,9 @@
+export default function RankingRoute() {
+  return (
+    <div>
+      <title>Spiele - runde.tips</title>
+      <meta name="description" content="Spiele der Haus23 Tipprunde" />
+      <h1 className="p-2 text-2xl font-medium">Spiele</h1>
+    </div>
+  );
+}
---
+++ app/routes/foh/players.tsx
@@ -0,0 +1,9 @@
+export default function RankingRoute() {
+  return (
+    <div>
+      <title>Spieler - runde.tips</title>
+      <meta name="description" content="Spieler der Haus23 Tipprunde" />
+      <h1 className="p-2 text-2xl font-medium">Spieler</h1>
+    </div>
+  );
+}
