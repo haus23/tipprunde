d9a81ca16db240d32f2952ff5512abd8f7989573
Micha Buchholz <micha@haus23.net>
2025-11-03 14:52:17 +0100

    feat: Add nav-link wrapper.

---
+++ app/components/ui/link.tsx
@@ -0,0 +1,11 @@
+import type { NavLinkProps } from "react-router";
+import { NavLink as _NavLink } from "react-router";
+import { cva } from "~/utils/cva";
+
+const navLinkClasses = cva({
+  base: ["flex items-center gap-2.5 px-1.5", "[&>svg]:size-5 [&>svg]:shrink-0"],
+});
+
+export function NavLink({ className, ...props }: NavLinkProps) {
+  return <_NavLink className={navLinkClasses({ className })} {...props} />;
+}
