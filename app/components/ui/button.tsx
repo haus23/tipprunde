18bb1063db8769043d0a1f103a562a36c1975cad
Micha Buchholz <micha@haus23.net>
2025-11-04 18:47:33 +0100

    refactor: Implement wrapper for rac button. Add initial primary variant.

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,9 +1,14 @@
+import {
+  Button as _Button,
+  type ButtonProps as _ButtonProps,
+} from "react-aria-components";
 import { cva, type VariantProps } from "~/utils/cva";
 
 const buttonClasses = cva({
-  base: [],
+  base: ["data-disabled:opacity-50"],
   variants: {
     variant: {
+      primary: "px-3 py-1.5 border",
       plain: "",
     },
   },
@@ -13,11 +18,11 @@ const buttonClasses = cva({
 });
 
 export interface ButtonProps
-  extends React.ComponentProps<"button">,
+  extends _ButtonProps,
     VariantProps<typeof buttonClasses> {}
 
 export function Button({ className, variant, ...props }: ButtonProps) {
   return (
-    <button className={buttonClasses({ className, variant })} {...props} />
+    <_Button className={buttonClasses({ className, variant })} {...props} />
   );
 }
