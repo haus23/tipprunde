6b7993fdac627da792a5291c39b016b0b086b086
Micha Buchholz <micha@haus23.net>
2025-11-04 13:38:02 +0100

    feat: Add tooltip to collapsed sidebar icons.

--- app/components/foh/nav.tsx
+++ app/components/foh/nav.tsx
@@ -1,22 +1,29 @@
 import { DicesIcon, TableIcon, UsersIcon } from "lucide-react";
 import { NavLink } from "~/components/ui/link";
+import { SidebarItem } from "../shell/sidebar-item";
 
 export function FohNav() {
   return (
     <div className="px-2 grow flex flex-col justify-between">
       <div className="flex flex-col gap-2">
-        <NavLink to="/">
-          <TableIcon />
-          <span>Tabelle</span>
-        </NavLink>
-        <NavLink to="/spieler">
-          <UsersIcon />
-          <span>Spieler</span>
-        </NavLink>
-        <NavLink to="/spiele">
-          <DicesIcon />
-          <span>Spiele</span>
-        </NavLink>
+        <SidebarItem tooltip="Tabelle">
+          <NavLink to="/">
+            <TableIcon />
+            <span>Tabelle</span>
+          </NavLink>
+        </SidebarItem>
+        <SidebarItem tooltip="Spieler">
+          <NavLink to="/spieler">
+            <UsersIcon />
+            <span>Spieler</span>
+          </NavLink>
+        </SidebarItem>
+        <SidebarItem tooltip="Spiele">
+          <NavLink to="/spiele">
+            <DicesIcon />
+            <span>Spiele</span>
+          </NavLink>
+        </SidebarItem>
       </div>
     </div>
   );
---
+++ app/components/shell/sidebar-item.tsx
@@ -0,0 +1,27 @@
+import { Focusable, Tooltip, TooltipTrigger } from "react-aria-components";
+import { useShell } from "./provider";
+import { cx } from "~/utils/cva";
+
+export function SidebarItem({
+  tooltip,
+  children,
+}: {
+  tooltip: string;
+  children: React.ReactElement<React.DOMAttributes<Element>, string>;
+}) {
+  const { isSidebarCollapsed } = useShell();
+  return (
+    <TooltipTrigger delay={750} isDisabled={!isSidebarCollapsed}>
+      <Focusable>{children}</Focusable>
+      <Tooltip
+        placement="right"
+        className={cx(
+          "rounded-sm bg-black px-2 py-1 text-white text-sm",
+          "mt-2",
+        )}
+      >
+        {tooltip}
+      </Tooltip>
+    </TooltipTrigger>
+  );
+}
