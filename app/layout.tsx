5af364a9c90723e3379cb913789979228e9274e6
Micha Buchholz <micha@haus23.net>
2026-03-06 08:30:30 +0100

    feat: Introduce color scheme.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="de">
-      <body className="relative isolate min-h-svh w-full">{children}</body>
+      <body className="bg-base relative isolate min-h-svh w-full text-base">{children}</body>
     </html>
   );
 }
