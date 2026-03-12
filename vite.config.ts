2679c2f6d74086e8280d9c6bb1873e8660f6bef3
Micha Buchholz <micha@haus23.net>
2026-03-12 23:30:29 +0100

    chore: Add nitro plugin to use nitro server for production.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { type UserConfig } from "vite";
 
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default {
-  plugins: [tailwindcss(), tanstackStart(), react()],
+  plugins: [tailwindcss(), tanstackStart(), nitro(), react()],
   resolve: {
     tsconfigPaths: true,
   },
