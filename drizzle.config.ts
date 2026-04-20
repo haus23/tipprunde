3ddd901df482a7b19d009538f75f73131caf55c9
Micha Buchholz <micha@haus23.net>
2026-04-20 14:10:42 +0200

    chore: Update drizzle-kit config and scripts.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "turso",
-  schema: "./db/schema/index.ts",
+  schema: "./db/schema/tables.ts",
   out: "./db/migrations",
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
