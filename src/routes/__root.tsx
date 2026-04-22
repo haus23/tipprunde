9112733a2c80c77387ffe2e87dee4c3cf34159b4
Micha Buchholz <micha@haus23.net>
2026-04-22 10:18:22 +0200

    feat: Read ui cookie and set color-scheme.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,6 +5,7 @@ import type React from "react";
 import { I18nProvider } from "react-aria-components";
 
 import { fetchUser } from "#/app/(auth)/session.ts";
+import { getUISettingsFn } from "#/app/settings/ui.ts";
 import { queryClient } from "#/utils/query-client.ts";
 
 import rootCss from "../styles/root.css?url";
@@ -23,8 +24,8 @@ export const Route = createRootRoute({
     links: [{ rel: "stylesheet", href: rootCss }],
   }),
   beforeLoad: async () => {
-    const user = await fetchUser();
-    return { user };
+    const [user, uiSettings] = await Promise.all([fetchUser(), getUISettingsFn()]);
+    return { user, uiSettings };
   },
   component: RootComponent,
 });
@@ -43,8 +44,9 @@ function RootComponent() {
 }
 
 function RootDocument({ children }: Readonly<{ children: React.ReactNode }>) {
+  const { uiSettings } = Route.useRouteContext();
   return (
-    <html lang="de">
+    <html lang="de" data-color-scheme={uiSettings.colorScheme}>
       <head>
         <HeadContent />
       </head>
