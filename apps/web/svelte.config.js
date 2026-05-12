21b3cd7724e0528de5f7fa6f6c658db38fe5b7fc
Micha Buchholz <micha@haus23.net>
2026-05-12 10:02:03 +0200

    chore: Prepare vercel build.

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,4 +1,10 @@
+import adapter from "@sveltejs/adapter-vercel";
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = {};
+const config = {
+  kit: {
+    adapter: adapter({}),
+  },
+};
 
 export default config;
