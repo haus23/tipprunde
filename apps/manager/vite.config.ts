4a32797e1bee223f8f3d67e71796e3254fa377c3
Micha Buchholz <micha@haus23.net>
2026-05-21 19:58:27 +0200

    chore: Align vite configuration.
    Signed-off-by: Micha Buchholz <micha@haus23.net>

--- apps/manager/vite.config.ts
+++ apps/manager/vite.config.ts
@@ -1,9 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { type UserConfig } from "vite";
 
-export default defineConfig({
+export default {
   build: { assetsDir: "manager/assets" },
   server: { port: 5174 },
   plugins: [reactRouter(), tailwindcss()],
-});
+} satisfies UserConfig;
