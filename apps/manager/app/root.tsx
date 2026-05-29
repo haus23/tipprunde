726c4dffa5c3c07e34eae672bf68ca41b7ae56ea
Micha Buchholz <micha@haus23.net>
2026-05-29 17:31:25 +0200

    fix(manager): Use explicit path for favicon.

--- apps/manager/app/root.tsx
+++ apps/manager/app/root.tsx
@@ -42,6 +42,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href={`${import.meta.env.BASE_URL}favicon.ico`} />
         <Meta />
         <Links />
       </head>
