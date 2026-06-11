2d3163fc6a49800ec15e3472fa81c0b8a991be23
Micha Buchholz <micha@haus23.net>
2026-06-11 20:30:46 +0200

    Switch port to be able to start legacy app separately.
    Signed-off-by: Micha Buchholz <micha@haus23.net>

--- apps/web-legacy/vite.config.ts
+++ apps/web-legacy/vite.config.ts
@@ -2,4 +2,7 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import type { UserConfig } from "vite";
 
-export default { plugins: [tailwindcss(), sveltekit()] } satisfies UserConfig;
+export default {
+  server: { port: 5175 },
+  plugins: [tailwindcss(), sveltekit()],
+} satisfies UserConfig;
