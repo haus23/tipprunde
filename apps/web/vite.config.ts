41fcfd48cbbf2d52b4adf201c6d85394446d8966
Micha Buchholz <micha@haus23.net>
2026-07-28 00:20:29 +0200

    refactor(web): Switch to cloudlfare deployment for the web app.
    Signed-off-by: Micha Buchholz <micha@haus23.net>

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,16 +1,16 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import optimizeLocales from "@react-aria/optimize-locales-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { type UserConfig } from "vite";
 
 export default {
   plugins: [
     { ...optimizeLocales.vite({ locales: ["de-DE"] }), enforce: "pre" },
     tailwindcss(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     react(),
-    nitro(),
   ],
 } satisfies UserConfig;
